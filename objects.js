a9fdff40acfcb7d36958030b517274d9836d7cb9
Julie Cook <juliecook328@gmail.com>
2019-06-14 01:04:49 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = { Queen: "Bohemian Phapsody" }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle
+  playlist.artistName = songTitle
   return playlist
 }
 function removeFromPlaylist(playlist, artistName) {

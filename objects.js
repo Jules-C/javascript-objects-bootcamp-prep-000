258e1ebff8018faa60026239a5e0dc30fda19fca
Julie Cook <juliecook328@gmail.com>
2019-06-14 00:51:18 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,7 +3,3 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, { [artistName]: songTitle })
 }
 
-function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle
-  return playlist
-}

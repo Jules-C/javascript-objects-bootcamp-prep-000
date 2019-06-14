844255fa2c33950e463cbc964406acb343c9ae84
Julie Cook <juliecook328@gmail.com>
2019-06-14 00:53:47 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,4 @@
 var playlist = { Queen: "Bohemian Phapsody" }
-function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, { [artistName]: songTitle })
-}
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
